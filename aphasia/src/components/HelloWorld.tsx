7b5eca0c4302f16f353e1b7c3bafb1046e695ac1
Potti1234 <lukas.pottner@gmail.com>
2025-10-11 17:59:45 +0200

    Install shadcn ui and remove unused parts of the code

--- aphasia/src/components/HelloWorld.tsx
+++ aphasia/src/components/HelloWorld.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {Button} from '@/components/ui/button'
 
 export default function HelloWorld(props: { msg: string }) {
   const [count, setCount] = useState(0)
@@ -8,11 +9,11 @@ export default function HelloWorld(props: { msg: string }) {
       <h1>{props.msg}</h1>
 
       <div className="card">
-        <button type="button" onClick={() => setCount(count + 1)}>
+        <Button type="button" onClick={() => setCount(count + 1)}>
           count is
           {' '}
           {count}
-        </button>
+        </Button>
         <p>
           Edit
           <code>src/components/HelloWorld.tsx</code>
